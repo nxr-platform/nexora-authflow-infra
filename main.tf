f5eae7508cdf333a4510109eb9a690e319a644de
Andrew Oppon <opponandrew@outlook.com>
2026-06-05 23:47:33 +0100

    feat(infra): wire Key Vault module into environment layer
    Adds Key Vault module call using data source for tenant_id.
    Exposes keyvault_id, keyvault_uri and keyvault_name as outputs.
    
    Refs: PLAT-028

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ module "resource_group" {
 
 module "keyvault" {
   source = "git::https://github.com/nxr-platform/nexora-authflow-terraform-modules.git//modules/keyvault?ref=v0.7.0"
- 
+
   resource_group_name           = module.resource_group.name
   company                       = local.company
   domain                        = local.domain
