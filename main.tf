bf67b7bec8c03afb62028dcfc353b06deb6759d5
Andrew Oppon <opponandrew@outlook.com>
2026-06-05 23:45:02 +0100

    feat(infra): wire Key Vault module into environment layer
    Adds Key Vault module call using data source for tenant_id.
    Exposes keyvault_id, keyvault_uri and keyvault_name as outputs.
    
    Refs: PLAT-028

--- main.tf
+++ main.tf
@@ -20,6 +20,8 @@ locals {
   })
 }
 
+data "azurerm_client_config" "current" {}
+
 module "resource_group" {
   source = "git::https://github.com/nxr-platform/nexora-authflow-terraform-modules.git//modules/resource-group?ref=v0.1.3"
 
@@ -31,4 +33,21 @@ module "resource_group" {
   tags        = local.common_tags
 }
 
+module "keyvault" {
+  source = "git::https://github.com/nxr-platform/nexora-authflow-terraform-modules.git//modules/keyvault?ref=v0.7.0"
  
+  resource_group_name           = module.resource_group.name
+  company                       = local.company
+  domain                        = local.domain
+  component                     = local.component
+  location                      = var.location
+  environment                   = var.environment
+  rbac_authorization_enabled    = var.rbac_authorization_enabled
+  soft_delete_retention_days    = var.soft_delete_retention_days
+  sku_name                      = var.sku_name
+  tenant_id                     = data.azurerm_client_config.current.tenant_id
+  public_network_access_enabled = var.public_network_access_enabled
+  purge_protection_enabled      = var.purge_protection_enabled
+  tags                          = local.common_tags
+}
+
--- output.tf
+++ output.tf
@@ -7,3 +7,18 @@ output "location" {
   description = "The location of the resource group"
   value       = module.resource_group.location
 }
+
+output "keyvault_id" {
+  description = "The ID of the Key Vault"
+  value       = module.keyvault.id
+}
+
+output "keyvault_uri" {
+  description = "The URI of the Key Vault"
+  value       = module.keyvault.uri
+}
+
+output "keyvault_name" {
+  description = "The name of the Key Vault"
+  value       = module.keyvault.name
+}
